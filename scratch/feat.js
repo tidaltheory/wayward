e498237ebb2e93a29b361c161887e3f82047a577
Jeff <rustyangel@gmail.com>
2014-08-26 19:45:43 -0600

    add feat object

---
+++ scratch/feat.js
@@ -0,0 +1,18 @@
+/**
+ * Feat object
+ */
+{
+  "id": 20,
+  "name": "",
+  "type": "Combat", // Normal, Combat, Metamagic, Teamwork
+  "prerequisites": [
+    {
+      "": ""
+    }
+  ],
+  "benefits": [
+    {
+      "": ""
+    }
+  ]
+}
