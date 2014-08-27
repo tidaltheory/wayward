b171343d81df48ab8e8a89dc582917e50e571f68
Jeff <rustyangel@gmail.com>
2014-08-26 20:06:20 -0600

    update feat `prerequisites` and `benefits` arrays

--- scratch/feat.js
+++ scratch/feat.js
@@ -3,16 +3,36 @@
  */
 {
   "id": 20,
-  "name": "",
+  "name": "Elemental Fist",
   "type": "Combat", // Normal, Combat, Metamagic, Teamwork
   "prerequisites": [
     {
-      "": ""
+      "thing": "stat",
+      "stat": "constitution",
+      "amount": 13
+    },
+    {
+      "thing": "stat",
+      "stat": "wisdom",
+      "amount": 13
+    },
+    {
+      "thing": "feat",
+      "feat": "Improved Unarmed Strike",
+      "feat_id": 9
+    },
+    {
+      "thing": "",
+      "": "Base Attack Bonus",
+      "amount": 8
     }
   ],
   "benefits": [
     {
-      "": ""
+      "benefit": "damage",
+      "attack_type": "Unarmed Strike",
+      "damage_dice": "1d6",
+      "damage_type": "energy"
     }
   ]
 }
